a132d734e51a5a1b39b7bf9e201e3d31e644ef40
Levi Halvorson <lhalvorson34@gmail.com>
2020-09-18 12:49:49 -0600

    added variables and charsets

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
+var passwordText = document.querySelector("#password");
+
+function writePassword() {
+
+var passLength = parseInt(prompt("How long do you want the password? (8-128)"));
+
+var hasUpper = confirm("Do you want Capital Letters?");
+var hasLower = confirm("Do you want Lowercase Letters?");
+var hasNumber = confirm("Do you want Numbers?");
+var hasSpecial = confirm("Do you want Special Characters?");
+
+var finalCharSet = '';
+if (hasUpper) finalCharSet += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+if (hasLower) finalCharSet += 'abcdefghijklmnopqrstuvwxyz'
+if (hasNumber) finalCharSet += '1234567890'
+if (hasSpecial) finalCharSet += '!@#$%^&*+='
+password = ''
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
