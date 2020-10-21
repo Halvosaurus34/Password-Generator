66b2d691984c1d77aad335e6bcb1cb811d30eba7
Levi Halvorson <lhalvorson34@gmail.com>
2020-10-20 19:52:34 -0600

    added more js

--- Develop/script.js
+++ Develop/script.js
@@ -4,38 +4,37 @@ generateBtn.addEventListener("click", writePassword);
 var passwordText = document.querySelector("#password");
 
 function writePassword() {
+  var passLength = parseInt(
+    prompt("How long do you want the password? (8-128)")
+  );
 
-var passLength = parseInt(prompt("How long do you want the password? (8-128)"));
+  var hasUpper = confirm("Do you want Capital Letters?");
+  var hasLower = confirm("Do you want Lowercase Letters?");
+  var hasNumber = confirm("Do you want Numbers?");
+  var hasSpecial = confirm("Do you want Special Characters?");
 
-var hasUpper = confirm("Do you want Capital Letters?");
-var hasLower = confirm("Do you want Lowercase Letters?");
-var hasNumber = confirm("Do you want Numbers?");
-var hasSpecial = confirm("Do you want Special Characters?");
+  //adds charset to final if true
+  var finalCharSet = "";
+  if (hasUpper) finalCharSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (hasLower) finalCharSet += "abcdefghijklmnopqrstuvwxyz";
+  if (hasNumber) finalCharSet += "1234567890";
+  if (hasSpecial) finalCharSet += "!@#$%^&*+=";
+  password = "";
 
-//adds charset to final if true
-var finalCharSet = '';
-if (hasUpper) finalCharSet += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-if (hasLower) finalCharSet += 'abcdefghijklmnopqrstuvwxyz'
-if (hasNumber) finalCharSet += '1234567890'
-if (hasSpecial) finalCharSet += '!@#$%^&*+='
-password = ''
-
-//picks random character for every index of password length
-if ( finalCharSet.length <=1){
-  alert("You must choose at least one password criteria!")
-  writePassword()
-} else {
-    if ( passLength > 7 && passLength < 129){
-      for (var i=0;i<passLength; i++){
-        var pass = Math.floor(Math.random() * finalCharSet.length)
-        password += finalCharSet[pass]
+  //picks random character for every index of password length
+  if (finalCharSet.length <= 1) {
+    alert("You must choose at least one password criteria!");
+    writePassword();
+  } else {
+    if (passLength > 7 && passLength < 129) {
+      for (var i = 0; i < passLength; i++) {
+        var pass = Math.floor(Math.random() * finalCharSet.length);
+        password += finalCharSet[pass];
       }
     } else {
-      alert("Password needs to be between 8 - 128")
-      writePassword()
+      alert("Password needs to be between 8 - 128");
+      writePassword();
     }
   }
-passwordText.value = password;
-
+  passwordText.value = password;
 }
-
